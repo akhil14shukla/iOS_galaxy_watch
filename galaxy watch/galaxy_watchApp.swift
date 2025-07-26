c2a692527ed2af989ad3179abf0f02a19e94ed26
akhil14shukla <akhil14shukla@gmail.com>
2025-07-26 13:37:48 +0530

    Initial Commit

---
+++ galaxy watch/galaxy_watchApp.swift
@@ -0,0 +1,17 @@
+//
+//  galaxy_watchApp.swift
+//  galaxy watch
+//
+//  Created by Akhil on 26/07/25.
+//
+
+import SwiftUI
+
+@main
+struct galaxy_watchApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ galaxy watchTests/galaxy_watchTests.swift
@@ -0,0 +1,17 @@
+//
+//  galaxy_watchTests.swift
+//  galaxy watchTests
+//
+//  Created by Akhil on 26/07/25.
+//
+
+import Testing
+@testable import galaxy_watch
+
+struct galaxy_watchTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
